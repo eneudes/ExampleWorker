7275f278de0dae5061c82e2d31180595ad9d0023
eneudes <eneudes.ap@gmail.com>
2019-02-15 16:29:01 -0300

    Dando inicio ao programa e estanciando as classes

--- ExampleWorker/Program.cs
+++ ExampleWorker/Program.cs
@@ -7,8 +7,19 @@ namespace ExampleWorker
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter department's name: ");
+            string nameDepartment = Console.ReadLine();
+            Department department = new Department { Name = nameDepartment };
+            Console.Write("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior, MidLvel, Senior): ");
+            string level = Console.ReadLine();
+            Console.WriteLine("Base Salary: ");
+            double salary = double.Parse(Console.ReadLine());
 
+            Console.WriteLine();
+            Console.Write("How many contracts to this worker? ");
 
         }
     }
