9d8d5dd00502ea1ef8c25440213f3a1c85deb66c
eneudes <eneudes.ap@gmail.com>
2019-02-15 16:44:13 -0300

    criando o laco de repeticao for para listar a quantidade de contratos

--- ExampleWorker/Program.cs
+++ ExampleWorker/Program.cs
@@ -10,6 +10,7 @@ namespace ExampleWorker
             Console.WriteLine("Enter department's name: ");
             string nameDepartment = Console.ReadLine();
             Department department = new Department { Name = nameDepartment };
+
             Console.Write("Enter worker data: ");
             Console.Write("Name: ");
             string name = Console.ReadLine();
@@ -20,7 +21,18 @@ namespace ExampleWorker
 
             Console.WriteLine();
             Console.Write("How many contracts to this worker? ");
+            int amount = int.Parse(Console.ReadLine());
 
+            for(int i = 1; i <= amount; i++)
+            {
+                Console.WriteLine("Enter #" + i + "contract data: ");
+                Console.WriteLine("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Value per hour: ");
+                double value = double.Parse(Console.ReadLine());
+                Console.WriteLine("Duration (hours): ");
+                int duration = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
